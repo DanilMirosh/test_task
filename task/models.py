4164f1724a778d9f664e91bdfb4c6f15d394075b
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2023-03-02 23:52:28 +0500

    fix app task

--- task/models.py
+++ task/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Название меню')
+
+    class Meta:
+        verbose_name = 'Меню'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return f'Меню {self.name}'
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Название элемента меню')
+    parent = models.ForeignKey(
+        'self',
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='child_items',
+        verbose_name='Родитель'
+    )
+    menu = models.ForeignKey(Menu, blank=True, on_delete=models.CASCADE, related_name='items', verbose_name='Меню')
+
+    class Meta:
+        verbose_name = 'Элемент меню'
+        verbose_name_plural = 'Элементы меню'
+
+    def __str__(self):
+        return f'Элемент: "{self.name}"'
--- task/views.py
+++ task/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+INDEX_HTML = 'task/index.html'
+
+
+def index(request):
+    return render(request, INDEX_HTML)
